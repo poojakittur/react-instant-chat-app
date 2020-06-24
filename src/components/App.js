7c8d28a49a54e167bad4d8eba59f5143de7c75c6
poojakittur <gitpoojakittur@gmail.com>
2020-06-24 02:43:47 -0500

    Adding login page

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
-require('../styles/App.css');
+require('../styles/App.scss');
 require('../styles/Login.css');
-
 import React from 'react';
 import ChatApp from './ChatApp';
 
@@ -9,6 +8,30 @@ import ChatApp from './ChatApp';
 class App extends React.Component {
   render() {
     // Display a simple login screen with a username field and a submit button
+
+    return (
+
+      // <div className="container">
+      //   <h1>Hello </h1>
+      //   <div className="col-lg-6"></div>
+      // </div>
+      <div className="container col-lg-12 loginBackground">
+        <div className="container col-lg-6 center-block loginContainer mt-5 p-4">
+          <form className="form-signin">       
+              <h2 className="form-signin-heading">Login</h2>
+                <div className="float-left col-lg-12 mb-3"><input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" /></div>
+                <div className="float-left col-lg-12 mb-3"><input type="password" class="form-control" name="password" placeholder="Password" required=""/> </div>     
+                <label class="checkbox">
+                    <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"/> Remember me
+                </label>
+                <button className="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
+            </form>
+        </div>
+      </div>
+      // <div className="loginForm">
+      //    
+      // </div>
+    )
   }
 
 }
@@ -16,3 +39,4 @@ App.defaultProps = {
 };
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 // This is where the App is bootstrapped.
+
 // We tell the ReactJs library that we want to display our main App component
 // inside a specific element in the DOM. In this case, there is a div with id="app"
 // inside the index.html file. Our React App component will be injected into here
